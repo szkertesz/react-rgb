0ceb1ab332c17f9157bfdff9a20ab11bee6270fb
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-08-30 14:43:12 +0200

    https://stevekinney.github.io/react-and-typescript/reducers-solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import { ColorSwatch } from './ColorSwatch'
+import { ColorInputs } from './ColorInputs'
+import { ColorSliders } from './ColorSliders'
+
+import { toRGB } from './utilities'
+import { reducer } from './reducer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [rgb, dispatch] = React.useReducer(reducer, {
+        red: 0,
+        green: 0,
+        blue: 0,
+    })
+    return (
+        <main style={{ borderColor: toRGB(rgb) }}>
+            <ColorSwatch {...rgb} />
+            <ColorInputs {...rgb} />
+            <ColorSliders {...rgb} dispatch={dispatch} />
+        </main>
+    )
 }
 
-export default App;
+export default App
